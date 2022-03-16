ace8240a0a83800a9721ef403ec627414865d303
Alan Bernstein <alan.bernstein@pm.me>
2022-03-16 12:35:12 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './styles/App.css';
+import Home from './routes/Home';
+
+function App() {
+  return (
+    <>
+      <Home/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+
+export default function Home() {
+  useEffect(() => {
+    const TYPE = 'sfwd-courses';
+    const URL = `${ process.env.REACT_APP_WP_DOMAIN }/${ process.env.REACT_APP_LD_EXT }/${ TYPE }`;
+
+    fetch(URL)
+      .then(response => response.json())
+      .then(data => {
+        for (const course of data) {
+          console.log('course', course);
+        }
+      })
+      .catch(console.error);
+  }, []);
+
+  return (
+    <></>
+  );
+}
