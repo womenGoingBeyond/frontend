5390008ff7325132647fb9d350aefb32123e1cc4
Alan Bernstein <alan.bernstein@pm.me>
2022-03-22 17:57:30 +0100

    add router and first form validation

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,16 +5,16 @@ export default function Home() {
     const TYPE = 'sfwd-courses'
     const URL = `${process.env.REACT_APP_WP_DOMAIN}/${process.env.REACT_APP_LD_EXT}/${TYPE}`
 
-    fetch(URL, {
-      mode: 'no-cors'
-    })
-      .then(response => response.json())
-      .then(data => {
-        for (const course of data) {
-          console.log('course', course)
-        }
-      })
-      .catch(console.error)
+    /*    fetch(URL, {
+          mode: 'no-cors'
+        })
+          .then(response => response.json())
+          .then(data => {
+            for (const course of data) {
+              console.log('course', course)
+            }
+          })
+          .catch(console.error)*/
   }, [])
 
   return (
