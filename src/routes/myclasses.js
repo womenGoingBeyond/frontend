2ff6a7de48c06dab9c30bb2dfbf434e6db69b327
Stefan Lorse <s.lorse@sprinteins.com>
2022-09-01 03:45:10 +0200

    change pre and lastname dynamically for certificates

--- src/routes/myclasses.js
+++ src/routes/myclasses.js
@@ -3,10 +3,12 @@ import Header from '../components/Header'
 import { useTranslation } from 'react-i18next'
 import Certificate from '../components/generateCertificate/certificate';
 import { PDFDownloadLink, PDFViewer } from '@react-pdf/renderer';
+import Data from '../util/data.js' 
 
 
 export default function MyClasses() {
   const {t, i18n} = useTranslation()
+  let commonData = Data.getInstance()
 
   const fileName = "Certificat-Name.pdf";
 
@@ -17,17 +19,18 @@ export default function MyClasses() {
     <main>
       <div className={mainStyles.container}>
       
-      <PDFViewer width={"400px"} height={"600px"} showToolbar={false}>
+      {/* <PDFViewer width={"400px"} height={"600px"} showToolbar={false}>
         <Certificate category="Kategsdfsfsdfsdfsdsad" course="fsfwefefwefwefwef wefwefwe fwefwewefef" nameOfStudent="name" certNumber="Asdsjd" certDate="234234234" />
-      </PDFViewer>
+      </PDFViewer> */}
 
       <div className='download-link'>
         <PDFDownloadLink
-          document={<Certificate  category="Kategsdfsfsdfsdfsdsad" course="fsfwefefwefwefwef wefwefwe fwefwewefef" nameOfStudent="name" certNumber="Asdsjd" certDate="234234234" />}
+        // nameOfStudent={commonData.getUserPrename() + " " + commonData.getUserLastname()}
+          document={<Certificate  category="Example Category" course="This is an Example Course" nameOfStudent="Max Mustermann" certNumber="100" certDate="07/22/2022" />}
           fileName={fileName}
         >
           {({ blob, url, loading, error }) =>
-            loading ? "Loading..." : "Download Certificate"
+            loading ? "Loading..." : "Download example certificate"
           }
         </PDFDownloadLink>
       </div>
